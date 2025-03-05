c97d875118bd2331597c5672fa3a51d27ee2aa79
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-03-05 13:54:45 -0600

    Implemented function ridge_post() for the variational inference vignette.

--- analysis/variational_inference_code.R
+++ analysis/variational_inference_code.R
@@ -1,6 +1,12 @@
 library(MASS)
 
-# TO DO: Explain what this function does and how to use it.
+set.seed(3)
+
+# Simulate n data points from a ridge regression model with p inputs.
+# Other parameters: p1, the number of nonzero coefficients to simulate
+# (should not be greater than p); s, the residual standard deviation
+# (s.d.); s0, the prior s.d. used to simulate the nonzero
+# coefficients; and r, the correlation among the inputs.
 sim_ridge_data <- function (n, p, p1, s = 1, s0 = 1, r = 0) {
   R <- matrix(r,p,p)
   diag(R) <- 1
@@ -13,9 +19,12 @@ sim_ridge_data <- function (n, p, p1, s = 1, s0 = 1, r = 0) {
   return(list(X = X,y = y,b = b))
 }
 
-# TO DO.
-ridge_post <- function () {
-  
+# Return the posterior distribution for the ridge regression model
+# given data X, y, residual s.d. s, and prior s.d. s0.
+ridge_post <- function (X, y, s, s0) {
+  p <- ncol(X)
+  V <- s^2 * solve(crossprod(X) + (s/s0)^2*diag(p))
+  return(list(cov = V,mean = drop(V %*% crossprod(X,y)/s^2)))
 }
 
 # TO DO.
@@ -23,6 +32,10 @@ ridge_gs <- function () {
 
 }
 
-sim <- sim_ridge_data(n = 500,p = 40,p1 = 2,s = 0.3,s0 = 2,r = 0.5)
+s <- 0.1
+s0 <- 2
+sim <- sim_ridge_data(n = 500,p = 40,p1 = 2,s = s,s0 = s0,r = 0.5)
 X <- sim$X
 y <- sim$y
+
+post <- ridge_post(X,y,s,s0)
